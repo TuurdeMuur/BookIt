95e37d97c131920a47b3b12113545c74af62c7ac
Tuur Mattheeuws <tuur.mattheeuws@student.howest.be>
2020-01-14 13:49:22 +0100

    aanpassingen book pagina

--- src/view/products/book.php
+++ src/view/products/book.php
@@ -2,9 +2,12 @@
 <h2 class="books__title">Kies je boek</h2>
 <p class="books__info"></p>
 <article class="books__book">
+<img src="books__book--img" alt="foto boek">
+<div class="wrapper">
 <p class="books__book--autor">Auteur</p>
   <h3 class="books__book--title">Title</h3>
   <p class="books__book--text"></p>
   <a class="books__book--order" href=""><img src="" alt="">Bestel</a>
+  </div>
 </article>
 </section>

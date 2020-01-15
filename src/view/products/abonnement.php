9f9cbd6d1c4cc7502ffa8b778ea9ea66a9545fc0
Tuur Mattheeuws <tuur.mattheeuws@student.howest.be>
2020-01-15 16:14:52 +0100

    css shop pagina

--- src/view/products/abonnement.php
+++ src/view/products/abonnement.php
@@ -1,8 +1,15 @@
 <section class="sub">
 <h2 class="sub__title">HUMO-abonnement</h2>
-<article class="books__book">
-<p class="books__book--autor">quote</p>
+<article class="sub__item">
+<img class="sub__item--img"   src="oldschool" alt="papieren versie">
+<p class="sub__item--quote">quote</p>
   <h3 class="books__book--title">Title</h3>
-  <a class="books__book--order" href=""><img src="" alt="">Bestel</a>
+  <a class="books__book--order" href="index.php?page=product&id=16"><img src="../assets/pijltje.svg" alt="pijltje">Bestel</a>
+</article>
+<article class="sub__item">
+<img class="sub__item--img"   src="radicaal digitaal" alt="papieren versie">
+<p class="sub__item--quote">quote</p>
+  <h3 class="books__book--title">Title</h3>
+  <a class="books__book--order" href="index.php?page=product&id=17"><img src="../assets/pijltje.svg" alt="pijltje">Bestel</a>
 </article>
 </section>
--- src/view/products/shop.php
+++ src/view/products/shop.php
@@ -1,76 +1,95 @@
 <section class="section section__reeks">
-  <h2 class="section__title" >HUMO-Boekenreeks <button></button></h2>
-  <article class="section__reeks--item product">
+  <div class="title__wrapper">
+  <h2 class="section__title" >HUMO-Boekenreeks</h2>
+  </div>
+  <div class="reeks-wrapper">
+  <article class="section__reeks--item boek product">
     <img class="product__img" src="../assets/boek.png" alt="boek">
     <p class="product__quote">Kies jouw favoriet</p>
     <h3 class="product__title">Elk boek apart</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=book">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=book">Bekijk</a></div>
    </article>
-   <article class="section__reeks--item product">
+   <article class="section__reeks--item ereader product">
    <img class="product__img" src="../assets/ereader.png" alt="boek">
     <p class="product__quote">Radicaal digitaal</p>
     <h3 class="product__title">E-book versie</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="">Bekijk</a></div>
    </article>
-   <article class="section__reeks--item product ">
+   <article class="section__reeks--item reeks product ">
     <img class="product__img" src="../assets/volledigereeks.png" alt="boek">
-    <p class="product__quote">UNIEKE-VERZAMELING</p>
-    <h3 class="product__title">Volledige reeks</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=13">Bekijk</a></div>
+    <div class="black">
+    <p class="product__quote quote-black">UNIEKE-VERZAMELING</p>
+    <h3 class="product__title title-black">Volledige reeks</h3>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje-wit.svg" alt="pijltje"><a class="link-black" href="index.php?page=product&id=13">Bekijk</a></div>
+    </div>
    </article>
-   <article class="section__reeks--item product">
+   <article class="section__reeks--item steun product">
    <img class="product__img" src="../assets/boekensteun.png" alt="boek">
     <p class="product__quote">opbergplezier</p>
     <h3 class="product__title">Boekensteun</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=2">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=product&id=2">Bekijk</a></div>
    </article>
-   <article class="section__reeks--item product">
+   <article class="section__reeks--item wijzer product">
    <img class="product__img" src="../assets/bladwijzer.png" alt="boek">
     <p class="product__quote">Leesplezier</p>
     <h3 class="product__title">Bladwijzer</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=1">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=product&id=1">Bekijk</a></div>
    </article>
-
+   </div>
 </section>
 <section class="section section__humo">
+  <div class="title__wrapper">
   <h2 class="section__title" >HUMO Gerelateerd</h2>
+  </div>
+  <div class="humo-wrapper">
   <article class="section__humo--item product">
-  <img class="product__img" src="../assets/abonnement.png" alt="boek">
+  <img class="product__img" src="../assets/abonnementen.png" alt="boek">
     <p class="product__quote">Wekelijks Thuis</p>
-    <h3 class="product__title">Humo abonnement</h3> <span>*digitale of papieren versie mogelijk</span>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=abonnement">Bekijk</a></div>
+    <div class="title-wrapper">
+    <h3 class="product__title">Humo abonnement</h3> <div>*digitale of papieren versie mogelijk</div>
+    </div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=abonnement">Bekijk</a></div>
    </article>
    <article class="section__humo--item product">
-   <img class="product__img" src="../assets/agenda.png" alt="boek">
+   <img class="product__img" src="../assets/agendas.png" alt="boek">
     <p class="product__quote">Organiseer jezelf</p>
     <h3 class="product__title">Humo agenda 2020</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=18">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=product&id=18">Bekijk</a></div>
    </article>
+   </div>
 </section>
 <section class="section section__gadgets">
+  <div class="title__wrapper">
   <h2 class="section__title" >Lees Gadgets</h2>
+  </div>
+  <div class="gadget-wrapper">
   <article class="section__gadget--item product">
-  <img class="product__img" src="../assets/leeslichtje.png" alt="boek">
-    <p class="product__quote">verlichting</p>
-    <h3 class="product__title">Leeslichtje</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje" ><a href="index.php?page=product&id=21">Bekijk</a></div>
+  <img class="product__img" src="../assets/leeslicht.png" alt="boek">
+  <div class="black">
+    <p class="product__quote quote-black">verlichting</p>
+    <h3 class="product__title title-black">Leeslichtje</h3>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje-wit.svg" alt="pijltje" ><a class="link-black" href="index.php?page=product&id=21">Bekijk</a></div>
+    </div>
    </article>
    <article class="section__gadget--item product">
-   <img class="product__img" src="../assets/loep.png" alt="boek">
+   <img class="product__img" src="../assets/loepglas.png" alt="boek">
     <p class="product__quote">onder de loep</p>
     <h3 class="product__title">Loep</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=19">Bekijk</a></div>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=product&id=19">Bekijk</a></div>
    </article>
    <article class="section__gadget--item product">
-   <img class="product__img" src="../assets/boekenlicht.png" alt="boek">
-    <p class="product__quote">Handig</p>
-    <h3 class="product__title">Mini boekenlicht</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=22">Bekijk</a></div>
+   <img class="product__img" src="../assets/boeklicht.png" alt="boek">
+   <div class="black">
+    <p class="product__quote quote-black">Handig</p>
+    <h3 class="product__title title-black">Mini <br> boekenlicht</h3>
+    <div class="product__link"><img class="product__link--img img-black" src="../assets/pijltje-wit.svg" alt="pijltje"><a class="link-black" href="index.php?page=product&id=22">Bekijk</a></div>
+    </div>
    </article>
    <article class="section__gadget--item product">
-   <img class="product__img" src="../assets/vergrootglas.png" alt="boek">
+   <img class="product__img" src="../assets/vergrootglass.png" alt="boek">
     <p class="product__quote">onder de loep</p>
-    <h3 class="product__title">Professioneel vergrootglas</h3>
-    <div class="product__link"><img src="../assets/pijltje.svg" alt="pijltje"><a href="index.php?page=product&id=20">Bekijk</a></div>
+    <h3 class="product__title">Professioneel <br> vergrootglas</h3>
+    <div class="product__link"><img class="product__link--img" src="../assets/pijltje.svg" alt="pijltje"><a class="product__link--a" href="index.php?page=product&id=20">Bekijk</a></div>
    </article>
+   </div>
 </section>

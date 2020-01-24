2f62d27823702f4583c6ac7b243f5bba1bea5023
Tuur Mattheeuws <tuur.mattheeuws@student.howest.be>
2020-01-24 10:08:09 +0100

    teksten controleren

--- src/view/products/long.php
+++ src/view/products/long.php
@@ -11,17 +11,17 @@
     <h2 class="level__title">Level 1</h2>
     </div>
     <img class="level__left" src="../assets/SVG/left.svg" alt="trap" width="1000">
-    <p class="level__text level__text--left">De Ernest Cline begon ooit als scenarist en wist meerdere van zijn scenario’s tot op the big screen te krijgen. Het kwam dan natuurlijk ook niet als een schok toen in 2010 slechts 1 dag na de publicatie van ready player one de filmrechten al werden opgekocht.</p>
+    <p class="level__text level__text--left">Ernest Cline begon ooit als scenarist en wist doorheen de jaren meerdere van zijn scenario’s tot op the big screen te krijgen. Het kwam dan natuurlijk ook niet als een schok toen in 2010 slechts één dag na de publicatie van ready player one de filmrechten al werden opgekocht.</p>
     <div class="level__right">
     <img class="level__right--img" src="../assets/SVG/right.svg" alt="trap" width="1000">
     </div>
     <div class="text__wrapper">
-    <p class="level__text level__text--right">Het boek zelf zit vol met verwijzingen naar Clines jeugd tijdens de jaren 80 in de states. De verwijzingen naar videogames en science-fiction films zij. Niet ver te zoeken aangezien deze in de jaren 80 als mainstream media werden gezien</p>
+    <p class="level__text level__text--right">Het boek zelf zit vol met verwijzingen naar Clines jeugd tijdens de jaren 80 in de states. Videogames en science-fiction films zijn nooit ver weg in het boek aangezien deze in de jaren 80 meer en meer als mainstream media werden gezien.</p>
     </div>
     <img class="level__left left-2" src="../assets/SVG/left_2.svg" alt="trap" >
   </article>
   <article class="level level__2">
-    <img class="pacman" src="../assets/SVG/pacman.svg" alt="packman" width="200">
+    <img class="pacman" src="../assets/SVG/pacman.svg" alt="packman" width="150">
     <div class="level__2-container">
     <div class ="pacman-wrapper">
     <img src="../assets/SVG/bolletjes.svg" alt="bolletjes" class="bolletjes">
@@ -29,11 +29,11 @@
     <img src="../assets/SVG/bolletjes.svg" alt="bolletjes" class="bolletjes">
     </div>
     <div class="level__2--wrapper">
-    <p class="level__text">Het boek die zich groten deels zelf in een virtuele Game wereld afspeelt is voor elke game liefhebber een genot om te lezen. De wereld refereert niet enkel naar bestaande games maar toont je ook een inkijk in de videogame cultuur als een geheel. </p>
+    <p class="level__text">Het boek die zich groten deels in een virtuele Game wereld afspeelt, is voor elke game liefhebber een genot om te lezen. De wereld verwijst niet enkel naar bestaande games maar toont je ook een inkijk in de videogame cultuur als een geheel.</p>
     <img src="../assets/SVG/bolletjes.svg" alt="bolletjes" class="bolletjes">
-    <p class="level__text">Dit wil natuurlijk niet zeggen dat het lezen van dit boek een goede videogame kennis vraagt. De alledaagse leesliefhebber zal dan misschien de subtiele hints of referenties niet altijd doorhebben des al niet te min blijven de spanning, intrige en personages universele geliefde elementen.</p>
+    <p class="level__text">Dit wil natuurlijk niet zeggen dat het lezen van dit boek een goede videogame kennis vraagt. De alledaagse leesliefhebber zal dan misschien  niet  altijdde subtiele hints of verwijzingen doorhebben des al niet te min blijven de spanning, intrige en personages universele geliefde elementen.</p>
     <img src="../assets/SVG/bolletjes.svg" alt="bolletjes" class="bolletjes" >
-    <p class="level__text">Een groot deel van het boek bestaat er ook uit dat de personages verscheidene easter eggs in de game moet zien te vinden. Easter eggs of te wel onverwachte of ongedocumenteerde features die vaak in games voorkomen zijn dan ook zelf in het boek verstopt en een zorgen er voor dat aandacht speurders een reden hebben om het boek een paar keer opnieuw door te lezen </p>
+    <p class="level__text">Een groot deel van het boek bestaat er ook uit dat de personages verscheidene easter eggs in de game moet zien te vinden. Easter eggs of te wel onverwachte of ongedocumenteerde features of verwijzingen die vaak in games voorkomen zijn dan ook in het boek zelf verstopt en een zorgen er voor dat aandacht speurders een reden hebben om het boek een paar keer opnieuw door te lezen.</p>
     </div>
     </div>
   <img class="pacman__mannetjes" src="assets/SVG/mannetje.svg" alt="pacman mannetjes" width="500">
@@ -42,7 +42,7 @@
     <div class="level__3-wrapper">
       <h2 class="level__title level__title--3">Level 3</h2>
     </div>
-    <p class="level__text film">Cline die zelf opgroeide in de tijd waarin franchises zoals star-wars, Indiana Jones en Back to the future in cinema’s domineerden zorgde ook zeker voor genoeg verwijzingen naar andere vormen van media. Film tv maar ook muziek komen allemaal aan bod in het boek. Uiteraard allemaal uit </p>
+    <p class="level__text film">Cline die zelf opgroeide in de tijd waarin franchises zoals star-wars, Indiana Jones en Back to the future in cinema’s domineerden zorgde ook zeker voor genoeg verwijzingen naar andere vormen van media. Film, tv maar ook muziek komen allemaal aan bod in het boek.</p>
     <div class="films-wrapper" >
       <p>Lijst van films die voorkomen in het boek:</p>
       <ul class="level__3--films">
@@ -63,22 +63,23 @@
   <article class="level level__4">
     <h2 class="level__title">Level 4</h2>
     <div class="level__4-wrapper">
-    <p class="level__text text-4">Als we het over Ready Player one het boek hebben kunnen we het onderwerp van de film uit 2018 niet uit de weg gaan. Zoals elk  boek die verfilmd werd is er ook bij dit boek een hele boel veranderd of weggelaten. Het feit dat je de film uit 2018 al gezien hebt mag dus zeker geen obstakel vormen om het boek alsnog te lezen. Het boek biedt nog zoveel meer en versterkt de beleving van het  kijken naar de film alleen maar.</p>
-    <p class="level__text text-4">Je kan het boek zijn als een veel uitgebreidere en gedetailleerde versie van de film. De verhaallijn die in grote lijnen wel hetzelfde is gebleven is gaat in het boek veel dieper in op de hoe en waarom en dit zorgt er dan weer voor dat je het gevoel hebt dat je een veel diepere connectie hebt met de personages die je volgt in het boek. Ook de popculture refferences zijn voor de film gemoderniseerd in tegenstelling tot die in het boek. Dit maakt het mogelijk om(indien je de film al eerder zag opnieuw op zoek te gaan naar de easter eggs en verborgen boodschappen in het boek.</p>
+    <p class="level__text text-4">Als we het over Ready Player one het boek hebben kunnen we het onderwerp van de film uit 2018 niet uit de weg gaan. Zoals elk boek die verfilmd werd is er ook bij dit boek een hele boel veranderd of weggelaten. Het feit dat je de film uit 2018 al gezien hebt,  zou  dus zeker geen obstakel vormen om het boek alsnog te lezen. Het boek biedt nog zoveel meer en versterkt de beleving van het kijken naar de film alleen maar.</p>
+    <p class="level__text text-4">Je kunt het boek zijn als een veel uitgebreidere en gedetailleerde versie van de film. De verhaallijn die in grote lijnen wel hetzelfde is gebleven is gaat in het boek veel dieper in op het hoe en waarom en dit zorgt er dan weer voor dat je het gevoel hebt dat je een veel diepere connectie hebt met de personages die je volgt in het boek. Ook de popcultuur reverences zijn voor de film gemoderniseerd in tegenstelling tot die in het boek. Dit maakt het mogelijk om (indien je de film al eerder zag) opnieuw op zoek te gaan naar de easter eggs en verborgen boodschappen in het boek.</p>
     </div>
   </article>
   <article class="level level__5">
     <h2 class="level__title">Level 5</h2>
     <div class="level__5-wrapper">
-   <p class="level__text two">Wie denkt dat Ready Player One een van de zovele science-fiction boeken is zonder veel inhoud of boodschap aan de lezer heeft het zeker mis. Het boek slaagt er in om de subtiele kritiek die het heeft op de samenleving ook in 2020 een 10 jaar na de eerste druk van het boek nog steeds relevant te zijn.  We kunnen zelf zeggen dat sommige aspecten die Cline aankaart in zijn boek gezien kunnen worden als een voorspelling van het heden.</p>
-    <p class="level__text three">Tot slot kunnen we zeggen dat Ready Player One een boek is die een groot publiek aanspreekt. Mensen die houden van het vinden van geheime boodschappen en liefhebbers van alles die 80’s is tot hardcore gamers. Bovenal is dit een boek voor mensen die houden van een goede roman boordevol avontuur en goede personages. Kortom Ready player One is een boek die niet te leur stelt voor eender welke lezer.</p>
+   <p class="level__text two">Wie denkt dat Ready Player One een van de zovele science-fiction boeken is zonder veel inhoud of boodschap aan de lezer heeft het zeker mis. Het boek slaagt er in om de subtiele kritiek die het heeft op de samenleving ook in 2020 een 10 jaar na de eerste druk van het boek nog steeds relevant te zijn. We kunnen zelf zeggen dat sommige aspecten die Cline aankaart in zijn boek gezien kunnen worden als een voorspelling van het nu.</p>
+    <p class="level__text three">Tot slot kunnen we zeggen dat Ready Player One een boek is die een groot publiek aanspreekt. Mensen die houden van het vinden van geheime boodschappen en liefhebbers van alles die 80’s is tot hardcore gamers. Bovenal is dit een boek voor mensen die houden van een goede roman boordevol avontuur en goede personages. Kortom Ready player One is een boek die niet te leur stelt voor eender welke lezer.
+</p>
     </div>
   </article>
   <article class="level level__6">
     <img class="joystick-gif" src="../assets/SVG/joystick.svg" alt="joystick">
     <h2 class="level__title">you died</h2>
     <div class="level__6-wrapper">
-    <p class="level__text">Bezoek onze shop om extra levens te bemachtigen. Ook het boe Ready player One en de andere boeken uit onze science-fiction reeks zijn beschikbaar in een exclusief uitgave apart of  als reeks.</p>
+    <p class="level__text">Vond jij al de easter eggs ? Laat het ons weten door een mail te sturen naar <span class="win">readyplayerone@humo.be</span> en win het boek. Indien je het boek of een van de andere boeken en nog zoveel meer wilt kopen bezoek dan zeker onze shop.</p>
     <a class="level__shop" href="index.php?page=shop">SHOP</a>
     </div>
   </article>
